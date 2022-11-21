befb38b0a542c7c6f5849ea6ee13d1b1f9ba95ad
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-21 15:39:05 +0100

    add discount count

--- js/script.js
+++ js/script.js
@@ -17,7 +17,28 @@ let costoBiglietto = numeroKm * 0.21;
 
 console.log(costoBiglietto);
 
-// Sconto per minorenni
+// Sconti
+
+let ventiPercento = costoBiglietto * 0.20;
+
+let costoBigliettoMinorenne = costoBiglietto - ventiPercento;
+
+let quarantaPercento = costoBiglietto * 0.40;
+
+let costoBigliettoOver = costoBiglietto - quarantaPercento;
+
+if ( (anniPasseggero > 18) && (anniPasseggero < 65) ){
+    document.getElementById('output').innerHTML = costoBiglietto.toFixed(2) + '&euro;';
+    console.log(costoBiglietto);
+} else if ( anniPasseggero < 18 ){
+    document.getElementById('output').innerHTML = costoBigliettoMinorenne.toFixed(2) + '&euro;';
+    console.log(costoBigliettoMinorenne);
+} else {
+    document.getElementById('output').innerHTML = costoBigliettoOver.toFixed(2) + '&euro;';
+    console.log(costoBigliettoOver);
+}
+
+
 
 
 
