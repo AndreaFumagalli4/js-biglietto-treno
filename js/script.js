bdf10fe9af982ba9c93b3ebfaf8e12a3c1644e24
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-21 15:11:14 +0100

    add ticket price per km

--- js/script.js
+++ js/script.js
@@ -3,11 +3,22 @@
 2. Età del passeggero/passeggera
 */
 
-const numeroKm = prompt ('Quanti chilometri farai?');
+const numeroKm = parseFloat (prompt ('Quanti chilometri farai?'));
 
 console.log(numeroKm);
 
-const anniPasseggero = prompt ('Quanti anni hai?');
+const anniPasseggero = parseFloat (prompt ('Quanti anni hai?'));
 
 console.log(anniPasseggero);
+
+// Prezzo del biglietto: 0.21€ al km
+
+let costoBiglietto = numeroKm * 0.21;
+
+console.log(costoBiglietto);
+
+// Sconto per minorenni
+
+
+
 
