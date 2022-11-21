d39337892fb53c8a2dbabb2b1790b92828ca1c77
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-21 15:02:52 +0100

    add richiesta km e età

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+1. Chiedere all'utente quanti km farà
+2. Età del passeggero/passeggera
+*/
+
+const numeroKm = prompt ('Quanti chilometri farai?');
+
+console.log(numeroKm);
+
+const anniPasseggero = prompt ('Quanti anni hai?');
+
+console.log(anniPasseggero);
+
